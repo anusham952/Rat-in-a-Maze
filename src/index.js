4310b7dc3b9d4ae2d37674c68607166e94ddeace
anusham952@gmail.com <anusham952@gmail.com>
2022-12-09 22:27:15 +0530

    Project made

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// import "./index.css";
+import App from "./App";
+
+import { DataProvider } from "./components/context/data-context";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+
+    <DataProvider>
+      <App />
+    </DataProvider>
+  
+);
+
+
